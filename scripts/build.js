97814c901d5a1d15ae04ab86311bd8558b6718dd
wayne-o <wayne@highstreet.ly>
2021-05-27 14:35:36 +0100

    Adding tailwindcss - not working

--- scripts/build.js
+++ scripts/build.js
@@ -13,6 +13,7 @@ const fs = require('fs').promises
 const path = require('path')
 const rollup = require('rollup')
 const packageJson = require('../package.json')
+const sveltePreprocess = require('svelte-preprocess')
 
 const appPath = path.resolve(__dirname, '../')
 const production = !process.env.ROLLUP_WATCH
@@ -30,191 +31,215 @@ const moduleFile = `${buildPath}/${displayModuleName}.js`
 const bundleName = `${displayModuleName}.${moduleVersion}.js`
 
 const outputOptions = {
-  sourcemap: true,
-  format: 'iife',
-  name: 'App',
-  file: `${buildPath}/${bundleName}`,
+    sourcemap: true,
+    format: 'iife',
+    name: 'App',
+    file: `${buildPath}/${bundleName}`,
 }
 
 const commonRollupPlugins = [
-  json(),
+    json(),
 
-  // If you have external dependencies installed from
-  // npm, you'll most likely need these plugins. In
-  // some cases you'll need additional configuration -
-  // consult the documentation for details:
-  // https://github.com/rollup/plugins/tree/master/packages/commonjs
-  nodeResolve({
-    browser: true,
-    dedupe: ['svelte'],
-  }),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    nodeResolve({
+        browser: true,
+        dedupe: ['svelte'],
+    }),
 
-  commonjs(),
+    commonjs(),
 
-  // transpile to ES2015+
-  babel({
-    extensions: ['.js', '.mjs', '.html', '.svelte'],
-    babelHelpers: 'runtime',
-  }),
+    // transpile to ES2015+
+    babel({
+        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        babelHelpers: 'runtime',
+    }),
 ]
 
 async function extractCSS() {
-  let cssChunk = ''
+    let cssChunk = ''
 
-  const bundle = await rollup.rollup({
-    input: entryPoint,
-    plugins: [
-      svelte({
-        compilerOptions: {
-          dev: false,
+    const bundle = await rollup.rollup({
+        input: entryPoint,
+        plugins: [
+            svelte({
+                compilerOptions: {
+                    dev: false,
 
-          // all nested child elements are built as normal svelte components
-          customElement: false,
-        },
-        emitCss: true,
-        preprocess: sveltePreprocess(),
-      }),
+                    // all nested child elements are built as normal svelte components
+                    customElement: false,
+                },
+                emitCss: true,
+                preprocess: sveltePreprocess({
+                    sourceMap: !production,
+                    postcss: {
+                        plugins: [
+                            require("tailwindcss"),
+                            require("autoprefixer"),
+                        ],
+                    },
+                }),
+            }),
 
-      // HACK! Inject nested CSS into custom element shadow root
-      css({
-        output(nestedCSS, styleNodes, bundle) {
-          const escapedCssChunk = nestedCSS
-            .replace(/\n/g, '')
-            .replace(/[\\"']/g, '\\$&')
-            .replace(/\u0000/g, '\\0')
+            // HACK! Inject nested CSS into custom element shadow root
+            css({
+                output(nestedCSS, styleNodes, bundle) {
+                    const escapedCssChunk = nestedCSS
+                        .replace(/\n/g, '')
+                        .replace(/[\\"']/g, '\\$&')
+                        .replace(/\u0000/g, '\\0')
 
-          cssChunk = escapedCssChunk
-        },
-      }),
+                    cssChunk = escapedCssChunk
+                },
+            }),
 
-      ...commonRollupPlugins,
-    ],
-  })
+            ...commonRollupPlugins,
+        ],
+    })
 
-  await bundle.generate(outputOptions)
+    await bundle.generate(outputOptions)
 
-  return cssChunk
+    return cssChunk
 }
 
 async function buildWebComponent({ minify, cssChunk }) {
-  // 1. Create a bundle
-  const bundle = await rollup.rollup({
-    input: entryPoint,
-    plugins: [
-      svelte({
-        compilerOptions: {
-          dev: false,
+    // 1. Create a bundle
+    const bundle = await rollup.rollup({
+        input: entryPoint,
+        plugins: [
+            svelte({
+                compilerOptions: {
+                    dev: false,
 
-          // all nested child elements are built as normal svelte components
-          customElement: false,
-        },
-        emitCss: true,
-        exclude: entryPointRegexp,
-        preprocess: sveltePreprocess(),
-      }),
-      svelte({
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: !production,
+                    // all nested child elements are built as normal svelte components
+                    customElement: false,
+                },
+                emitCss: true,
+                exclude: entryPointRegexp,
+                preprocess: sveltePreprocess({
+                    sourceMap: !production,
+                    postcss: {
+                        plugins: [
+                            require("tailwindcss"),
+                            require("autoprefixer"),
+                        ],
+                    },
+                }),
+            }),
+            svelte({
+                compilerOptions: {
+                    // enable run-time checks when not in production
+                    dev: !production,
 
-          // we're generating a -- Web Component -- from index.svelte
-          customElement: true,
-        },
-        emitCss: false,
-        include: entryPointRegexp,
-        preprocess: sveltePreprocess(),
-      }),
+                    // we're generating a -- Web Component -- from index.svelte
+                    customElement: true,
+                },
+                emitCss: false,
+                include: entryPointRegexp,
+                preprocess: sveltePreprocess({
+                    sourceMap: !production,
+                    postcss: {
+                        plugins: [
+                            require("tailwindcss"),
+                            require("autoprefixer"),
+                        ],
+                    },
+                }),
+            }),
 
-      // HACK! Inject nested CSS into custom element shadow root
-      css({
-        output(nestedCSS, styleNodes, bundle) {
-          const code = bundle[bundleName].code
+            // HACK! Inject nested CSS into custom element shadow root
+            css({
+                output(nestedCSS, styleNodes, bundle) {
+                    const code = bundle[bundleName].code
 
-          const matches = code.match(
-            minify
-              ? /.shadowRoot.innerHTML='<style>(.*)<\/style>'/
-              : /.shadowRoot.innerHTML = "<style>(.*)<\/style>"/,
-          )
+                    const matches = code.match(
+                        minify
+                            ? /.shadowRoot.innerHTML='<style>(.*)<\/style>'/
+                            : /.shadowRoot.innerHTML = "<style>(.*)<\/style>"/,
+                    )
 
-          if (matches && matches[1]) {
-            const style = matches[1]
-            bundle[bundleName].code = code.replace(style, cssChunk)
-          } else {
-            throw new Error(
-              "Couldn't shadowRoot <style> tag for injecting styles",
-            )
-          }
-        },
-      }),
+                    if (matches && matches[1]) {
+                        const style = matches[1]
+                        bundle[bundleName].code = code.replace(style, cssChunk)
+                    } else {
+                        throw new Error(
+                            "Couldn't shadowRoot <style> tag for injecting styles",
+                        )
+                    }
+                },
+            }),
 
-      // HACK! Fix svelte/transitions in web components
+            // HACK! Fix svelte/transitions in web components
 
-      // Use shadow root instead of document for transition style injecting
-      replace({
-        '.ownerDocument': '.getRootNode()',
-        delimiters: ['', ''],
-      }),
+            // Use shadow root instead of document for transition style injecting
+            replace({
+                '.ownerDocument': '.getRootNode()',
+                delimiters: ['', ''],
+            }),
 
-      // Append styles to shadow root
-      replace({
-        '.head.appendChild(e': '.appendChild(e',
-        delimiters: ['', ''],
-      }),
+            // Append styles to shadow root
+            replace({
+                '.head.appendChild(e': '.appendChild(e',
+                delimiters: ['', ''],
+            }),
 
-      // END HACK
+            // END HACK
 
-      ...commonRollupPlugins,
+            ...commonRollupPlugins,
 
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload(buildPath),
+            // Watch the `public` directory and refresh the
+            // browser on changes when not in production
+            !production && livereload(buildPath),
 
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      minify && terser(),
-    ],
-  })
+            // If we're building for production (npm run build
+            // instead of npm run dev), minify
+            minify && terser(),
+        ],
+    })
 
-  // 2. Generate output specific code in-memory
-  // you can call this function multiple times on the same bundle object
-  const { output } = await bundle.generate(outputOptions)
-  const { code, map } = output[0]
+    // 2. Generate output specific code in-memory
+    // you can call this function multiple times on the same bundle object
+    const { output } = await bundle.generate(outputOptions)
+    const { code, map } = output[0]
 
-  // 3. Write bundles into files
-  const fileName = minify
-    ? `${outputOptions.file.replace('.js', '.min.js')}`
-    : outputOptions.file
+    // 3. Write bundles into files
+    const fileName = minify
+        ? `${outputOptions.file.replace('.js', '.min.js')}`
+        : outputOptions.file
 
-  // Normal bundle
-  await fs.writeFile(fileName, code)
+    // Normal bundle
+    await fs.writeFile(fileName, code)
 
-  // Only create .map.js when code is minified
-  if (minify) {
-    await fs.writeFile(fileName.replace('.js', '.js.map'), map.toString())
-  }
+    // Only create .map.js when code is minified
+    if (minify) {
+        await fs.writeFile(fileName.replace('.js', '.js.map'), map.toString())
+    }
 
-  // Generic bundle for using as module entry point
-  if (!minify) {
-    await fs.writeFile(moduleFile, code)
-  }
+    // Generic bundle for using as module entry point
+    if (!minify) {
+        await fs.writeFile(moduleFile, code)
+    }
 }
 
 async function main() {
-  try {
-    shell.mkdir('-p', buildPath)
-    shell.rm('dist/*')
+    try {
+        shell.mkdir('-p', buildPath)
+        shell.rm('dist/*')
 
-    const cssChunk = await extractCSS()
+        const cssChunk = await extractCSS()
 
-    // builds readable bundle of the web component
-    await buildWebComponent({ minify: false, cssChunk })
+        // builds readable bundle of the web component
+        await buildWebComponent({ minify: false, cssChunk })
 
-    // builds minified bundle with sourcemap
-    await buildWebComponent({ minify: true, cssChunk })
-  } catch (ex) {
-    console.error(ex)
-    process.exit(1)
-  }
+        // builds minified bundle with sourcemap
+        await buildWebComponent({ minify: true, cssChunk })
+    } catch (ex) {
+        console.error(ex)
+        process.exit(1)
+    }
 }
 
 main()
--- src/index.svelte
+++ src/index.svelte
@@ -1,69 +1,37 @@
 <svelte:options tag="svelte-custom-element" />
 
 <script>
-  import { createEventDispatcher, onDestroy, onMount } from 'svelte'
-  import { fade } from 'svelte/transition'
-  import { isLoading, t, locale } from 'svelte-i18n'
-  import setupI18N from './lib/i18n'
-  import Clock from './clock/Clock.svelte'
-
-  export let title = 'Hello from component!!'
-  let initialized = false
-  let visible = true
-
-  const dispatch = createEventDispatcher()
-
-  const unsuscribeLangChange = locale.subscribe(lang => {
-    if (lang) {
-      dispatch('language-change', lang)
-    }
-  })
-
-  const fontStyleNode = document.createElement('link')
-
-  onMount(() => {
-    // HACK: Font import doesn't work inside shadow-dom.
-    // Read more: https://stackoverflow.com/questions/55382081/using-google-fonts-with-shadow-dom
-    fontStyleNode.type = 'text/css'
-    fontStyleNode.rel = 'stylesheet'
-    fontStyleNode.href =
-      '//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap'
-    document.head.appendChild(fontStyleNode)
-
-    setupI18N()
-    initialized = true
-  })
-
-  onDestroy(() => {
-    unsuscribeLangChange()
-
-    // HACK: Font import doesn't work inside shadow-dom.
-    document.head.removeChild(fontStyleNode)
-  })
-
-  const changeLang = lang => () => locale.set(lang)
 </script>
 
-{#if $isLoading || !initialized}
-  <p>Please wait...</p>
-{:else}
-  <div class="component-styles-wrapper">
-    <h1>{title}</h1>
+<nav class="bg-blue-900 shadow-lg">
+  <div class="container mx-auto">
+    <div class="sm:flex">
+      <a href="#" class="text-white text-3xl font-bold p-3">APP LOGO</a>
 
-    <p>{$t('text:example-paragraph')}</p>
-
-    <button on:click={changeLang('en')}>EN</button>
-    <button on:click={changeLang('es')}>ES</button>
-
-    <button on:click={() => (visible = !visible)}>Show/hide clocky</button>
-
-    {#if visible}
-      <div class="clock-container" transition:fade>
-        <Clock />
+      <!-- Menus -->
+      <div class="ml-55 mt-4">
+        <ul class="text-white sm:self-center text-xl">
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">About</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Services</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Blog</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Contact</a>
+          </li>
+        </ul>
       </div>
-    {/if}
+    </div>
   </div>
-{/if}
+</nav>
 
-<style global type="text/scss" lang="scss" src="./index.scss">
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
 </style>
