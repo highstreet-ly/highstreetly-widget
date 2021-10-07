b7978d5e9abfba968d5dd1bb23208735f652102c
wayne-o <wayne@highstreet.ly>
2021-10-07 09:54:18 +0100

    blurgh

--- src/index.stories.js
+++ src/index.stories.js
@@ -8,13 +8,13 @@ export const CustomElementStoryWithProps = () => ({
         // stripe: 'pk_test_KLsvb9WFdUIEjkyEv0UyefHQ',
         // api: 'https://api.shop.highstreet.ly/api/v1/',
         // event: '3e990000-412a-ee9b-e228-08d8faab4e5b'
-        stripe: 'pk_test_KLsvb9WFdUIEjkyEv0UyefHQ',
-        api: 'https://api.shop.sonashop.xyz/api/v1/',
-        event: '628f0000-be38-06be-45aa-08d92cd503e8'
+        // stripe: 'pk_test_KLsvb9WFdUIEjkyEv0UyefHQ',
+        // api: 'https://api.shop.sonashop.xyz/api/v1/',
+        // event: '628f0000-be38-06be-45aa-08d92cd503e8'
 
-        /*stripe: 'pk_test_KLsvb9WFdUIEjkyEv0UyefHQ',
-        api: 'https://api.shop.highstreetly.live/api/v1/',
-        event: '744c0000-3bfb-ea3b-f8df-08d95e601284'*/
+        stripe: 'pk_test_KLsvb9WFdUIEjkyEv0UyefHQ',
+        api: 'https://api.shop.highstreetly.xyz/api/v1/',
+        event: 'b30a0000-d88b-7e1b-a99a-08d9790cd2f0'
         
     },
 })
